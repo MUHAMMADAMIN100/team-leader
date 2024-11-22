79aa97277ac80d0856db7a219c10fed8c244bb7c
MUHAMMADAMIN100 <turazodamuhammadamin@gmail.com>
2024-11-22 08:07:42 +0300

    header fixed

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,22 @@
+import './header.css'
+import HeaderImg from './images/Logo (5).png'
+import HeaderIcon from './images/Icon (7).png'
+
 export function Header()
 {
     return <>
+
+        <div className="HeaderDiv">
+            <div className="HeaderLogo"><img src={HeaderImg} alt="" /></div>
+            <div className="HeaderTexts">
+                <p className='HeaderPText'>Home</p>
+                <p className='HeaderPText'>About</p>
+                <p className='HeaderPText'>Blog</p>
+                <p className='HeaderPText'>Pricing</p>
+                <p className='HeaderPText HeaderContact'>Contact Us</p>
+            </div>
+            <img className='HeaderIcon' src={HeaderIcon} alt="" />
+        </div>
     
-    <h1>Header</h1>
     </>    
 }
